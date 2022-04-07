55d3b3623a15ad6708fbd436d2a60118d6450357
Renannevesr <renannevesr@gmail.com>
2022-04-06 21:06:13 -0300

    Atualização tratamento de datas

--- adicao_feriados.py
+++ adicao_feriados.py
@@ -4,15 +4,21 @@ from IPython.display import display
 
 balanco = pd.read_excel("balancos/BBAS3.xls")
 display(balanco)
-datas = balanco.loc[0]
-datas = pd.DataFrame(datas)
-datas.columns = ['Data']
-print(datas)
-datas.to_excel(f'datas.xlsx', header=True, index=False)
+df = balanco.loc[0]
+df = pd.DataFrame(df)
+df.columns = ['Data']
+# print(df)
+#df.to_excel(f'datas.xlsx', header=True, index=False)
 
+df['Data'] = pd.to_datetime(df['Data'])
+print(df.dtypes)
+print(df)
+
+ac = pd.read_excel("Cotacoes_final.xlsx")
+print(ac.dtypes)
 #acoes = pd.read_excel("Cotacoes_BBAS3.xlsx")
 
-#acoes = acoes.append(datas)
+#acoes = acoes.append(df)
 # display(acoes)
 #acoes.to_excel(f'teste1.xlsx', header=True, index=True)
 #df.loc[df['Veículo'] == 'Fiat', 'Veículo'] = 0
