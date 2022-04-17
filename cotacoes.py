25a56d738a9f595e32e475be9336c28bb10be8c6
Renannevesr <renannevesr@gmail.com>
2022-04-16 23:24:08 -0300

    adição de feriados e otimização do código

--- cotacoes.py
+++ cotacoes.py
@@ -1,51 +1,48 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 66f528eea6595ae87d3dd96299a012019979639e
 from pandas_datareader import data as web
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 empresas = pd.read_excel("empresas.xlsx")
-# display(empresas)
-df = [(('High', 'Low', 'Open', 'Close', 'Volume', 'Adj Close'))]
-df = pd.DataFrame(df, columns=['High', 'Low',
-                  'Open', 'Close', 'Volume', 'Adj Close'])
+df = []
+df = pd.DataFrame(df)
+cota_final = []
+cota_final = pd.DataFrame(df)
 df_emp = [(('Empresa'))]
 df_emp = pd.DataFrame(df_emp, columns=['Empresa'])
 
 for empresa in empresas['Empresas']:
-    # print(empresa)
-
     try:
         cotacoes = web.DataReader(f'{empresa}.SA', data_source='yahoo',
                                   start="01-01-2013", end="03-30-2022")
         df = df.append(cotacoes)
-        # display(cotacoes)
-        # print(empresa)
-        #pd.concat([df, df_emp])
-        # display(df)
-        df.to_excel(f'Cotacoes_{empresa}.xlsx',
-                    sheet_name='Acoes', header=True, index=True)
+        df['Data'] = df.index
+        balanco = pd.read_excel(f"balancos/{empresa}.xls")
+        datas = balanco.loc[0]
+        datas = pd.DataFrame(datas)
+        datas.columns = ['Data']
+        datas['Data'] = pd.to_datetime(datas['Data'])
+        df['Data'] = pd.to_datetime(df['Data'])
+        cota = pd.concat([df, datas]).sort_values(by='Data').drop_duplicates()
+        # cota.to_excel(f'Cotacoes_{empresa}.xlsx',
+        #              sheet_name='Acoes', header=True)
+        nova_cota = cota.assign(Nome=empresa)
+        cota_final = cota_final.append(cota)
     except:
         print(f"Não foi possível localizar os dados da {empresa}")
-    #cotacoes["Adj Close"].plot()
-    # plt.show()
-for empresa in empresas['Empresas']:
-    cota = pd.read_excel(f"Cotacoes_{empresa}.xlsx")
-    nova_cota = cota.assign(Nome=empresa)
-    # display(nova_cota)
-    nova_cota.to_excel(f'Cotacoes_{empresa}.xlsx',
-                       sheet_name='Acoes', header=True, index=False)
-cota_final = pd.DataFrame()
-for empresa in empresas['Empresas']:
-    #print("ultimo for ", empresa)
-    cota_final = cota_final.append(pd.read_excel(f"Cotacoes_{empresa}.xlsx"))
-    # display(cota_final)
+
+# for empresa in empresas['Empresas']:
+#    cota = pd.read_excel(f"Cotacoes_{empresa}.xlsx")
+#    nova_cota = cota.assign(Nome=empresa)
+#    nova_cota.to_excel(f'Cotacoes_{empresa}.xlsx',
+#                       sheet_name='Acoes', header=True, index=False)
+#cota_final = pd.DataFrame()
+# for empresa in empresas['Empresas']:
+#    cota_final = cota_final.append(pd.read_excel(f"Cotacoes_{empresa}.xlsx"))
 high = ['High']
 cota_final = cota_final.drop(cota_final[cota_final['High'].isin(high)].index)
-cota_final.columns = ['Data', 'High', 'Low', 'Open',
-                      'Close', 'Volume', 'Adj Close', 'Empresa']
+# cota_final.columns = ['Duplicada', 'High', 'Low', 'Open',
+#                      'Close', 'Volume', 'Adj Close', 'Data', 'Empresa']
+# cota_final.drop(columns=['Duplicada'])
 cota_final.to_excel(f'Cotacoes_final.xlsx',
                     sheet_name='Acoes', header=True, index=False)
